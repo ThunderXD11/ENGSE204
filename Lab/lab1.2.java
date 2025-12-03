e63c77945d641cddc2638668b89f4f43a36efeb4
ThunderXD <worasit.b2547@gmail.com>
2025-12-03 23:04:51 +0700

    Update lab1.2.java

--- Lab/lab1.2.java
+++ Lab/lab1.2.java
@@ -2,17 +2,19 @@ package lab1;
 
 import java.util.Scanner;
 
-public class lab12 {
+public class lab1_2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter Width: ");
         double width = scanner.nextDouble();
 
+        System.out.print("Enter Height: ");
         double height = scanner.nextDouble();
 
         double area = width * height;
 
-        System.out.println(area);
+        System.out.println("Area: " + area);
         
         scanner.close();
     }
