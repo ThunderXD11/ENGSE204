53f56fc81a1ea6075b1c4a86dc2cf64169c71fb9
ThunderXD <worasit.b2547@gmail.com>
2025-12-10 23:18:06 +0700

    Create lab3.2.java

---
+++ Lab/lab3.2.java
@@ -0,0 +1,36 @@
+package lab3;
+
+import java.util.Scanner;
+
+class Player {
+
+    private int score;   // attribute แบบ private
+
+    public Player(int initialScore) {
+        this.score = initialScore;
+    }
+
+    public int getScore() {
+        return this.score;
+    }
+
+    public void setScore(int newScore) {
+        this.score = newScore;
+    }
+}
+
+public class lab3_2 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int initialScore = sc.nextInt();
+        int updatedScore = sc.nextInt();
+
+        Player p = new Player(initialScore);
+
+        p.setScore(updatedScore);
+
+        System.out.println(p.getScore());
+    }
+}
