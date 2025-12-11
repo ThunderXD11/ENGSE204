5bfaa5cd71f9b0bdb27ed3bd88c21083e3fbb4e7
ThunderXD <worasit.b2547@gmail.com>
2025-12-11 23:45:29 +0700

    Update lab3.2.java

--- Lab/lab3.2.java
+++ Lab/lab3.2.java
@@ -3,8 +3,7 @@ package lab3;
 import java.util.Scanner;
 
 class Player {
-
-    private int score;   // attribute แบบ private
+    private int score;
 
     public Player(int initialScore) {
         this.score = initialScore;
@@ -20,17 +19,21 @@ class Player {
 }
 
 public class lab3_2 {
+
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-
-        int initialScore = sc.nextInt();
-        int updatedScore = sc.nextInt();
-
-        Player p = new Player(initialScore);
-
-        p.setScore(updatedScore);
-
-        System.out.println(p.getScore());
+        try (Scanner sc = new Scanner(System.in)) { 
+            
+            int initialScore = sc.nextInt();
+            
+            int updatedScore = sc.nextInt();
+            
+            Player p = new Player(initialScore);
+            
+            p.setScore(updatedScore);
+            
+            System.out.println(p.getScore());
+            
+        } 
     }
 }
